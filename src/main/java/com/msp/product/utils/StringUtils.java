67455a24b017e2185c91dc0dcd18291516a0efc6
Ricardo Luiz Silveira <ricardo.silveira@gmail.com>
2021-03-06 23:43:43 -0300

    Util class

---
+++ src/main/java/com/msp/product/utils/StringUtils.java
@@ -0,0 +1,9 @@
+package com.msp.product.utils;
+
+public class StringUtils {
+
+    public static String toUpperCase(String obj){
+        if (obj == null) return null;
+        return obj.toUpperCase();
+    }
+}
